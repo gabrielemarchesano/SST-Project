6a6be4d76e0df292e32650722fb2c19cc955e157
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-12-17 15:47:52 +0100

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,20 @@ function App() {
 
   return (
     <>
-      <h1>Web app</h1>
-      {
-        console.log(viaggi)
-      }
+
+      <header>
+        <nav className="navbar bg-body-tertiary px-4">
+          <a className="navbar-brand" href="#">
+            <i className="bi bi-airplane-fill pe-2"></i>Viaggi
+          </a>
+          <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </div>
+        </nav>
+      </header>
+
+      <main></main>
+      <footer></footer>
     </>
   )
 }
